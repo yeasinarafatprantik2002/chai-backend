e5504974985b2f04f72154aec3e389387bbab0d1
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-08-02 04:19:22 +0600

    feat: Add subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema(
+    {
+        subscriber: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        channel: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true } 
+);
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export { Subscription };
