1e6a7056152446d58eb1055ff452b6688c84c2bf
yeasinarafatprantik2002 <yeasinarafatprantik@gmail.com>
2024-08-12 11:35:44 +0600

    refactor: Add toggle functionality for liking videos, comments, and tweets
    This commit adds the toggle functionality for liking videos, comments, and tweets in the like.controller.js file. It includes the implementation for toggling likes on videos, comments, and tweets based on the provided videoId, commentId, and tweetId respectively. The code also handles error cases such as invalid object IDs and ensures that only the owner of the like can toggle it. The response messages are improved for better user experience.

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -6,21 +6,141 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
-    //TODO: toggle like on video
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    const like = await Like.findOne({ video: videoId, likedBy: req.user._id });
+
+    if (like) {
+        await Like.deleteOne({ video: videoId, likedBy: req.user._id });
+        return res.status(200).json(new ApiResponse(200, {}, "Video unliked"));
+    }
+
+    await Like.create({ video: videoId, likedBy: req.user._id });
+    return res.status(200).json(new ApiResponse(200, {}, "Video liked"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
     const { commentId } = req.params;
-    //TODO: toggle like on comment
+
+    if (!isValidObjectId(commentId)) {
+        throw new ApiError(400, "Invalid comment id");
+    }
+
+    const like = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id,
+    });
+
+    if (like) {
+        await Like.findByIdAndDelete(like._id);
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "Comment unliked"));
+    }
+
+    await Like.create({ comment: commentId, likedBy: req.user._id });
+    return res.status(200).json(new ApiResponse(200, {}, "Comment liked"));
 });
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
     const { tweetId } = req.params;
-    //TODO: toggle like on tweet
+
+    if (!isValidObjectId(tweetId)) {
+        throw new ApiError(400, "Invalid tweet id");
+    }
+
+    const like = await Like.findOne({ tweet: tweetId, likedBy: req.user._id });
+
+    if (like) {
+        await Like.findByIdAndDelete(like._id);
+        return res.status(200).json(new ApiResponse(200, {}, "Tweet unliked"));
+    }
+
+    await Like.create({ tweet: tweetId, likedBy: req.user._id });
+    return res.status(200).json(new ApiResponse(200, {}, "Tweet liked"));
 });
 
 const getLikedVideos = asyncHandler(async (req, res) => {
-    //TODO: get all liked videos
+    const likedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user._id),
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "video",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                        },
+                    },
+                    {
+                        $project: {
+                            _id: 1,
+                            title: 1,
+                            description: 1,
+                            videoFile: 1,
+                            thumbnail: 1,
+                            duration: 1,
+                            views: 1,
+                            isPublished: 1,
+                            owner: {
+                                _id: 1,
+                                username: 1,
+                                email: 1,
+                                fullName: 1,
+                                avatar: 1,
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+
+        {
+            $lookup: {
+                from: "users",
+                localField: "likedBy",
+                foreignField: "_id",
+                as: "likedBy",
+                pipeline: [
+                    {
+                        $project: {
+                            _id: 1,
+                            username: 1,
+                            email: 1,
+                            fullName: 1,
+                            avatar: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $unwind: "$video",
+        },
+    ]);
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                likedVideos,
+                "Liked videos fetched successfully"
+            )
+        );
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
